a65ce19b165788e0797da16efd1a648b1e4d0c21
abdibrokhim <abdibrokhim@gmail.com>
2023-04-07 09:14:43 +0500

    init

---
+++ client/src/App.js
@@ -0,0 +1,228 @@
+import React from 'react';
+import './App.css';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { teal } from '@mui/material/colors';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import CloseIcon from '@mui/icons-material/Close';
+
+
+function App() {
+  const [gameUrl, setGameUrl] = React.useState('');
+  const [gameScore, setGameScore] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [alertText, setAlertText] = React.useState({ message: '', status: '' });
+
+
+  const apiUrl = "https://tggameehacker-api.ba-students.uz/api/update_score/";
+  const apiKey = "";
+
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+        return;
+    }
+
+    setOpen(false);
+  };
+
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+
+  const Stackbar = () => {
+    return (
+      <Stack spacing={2} sx={{ width: '100%' }}>
+      <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity={alertText.status} sx={{ width: '100%', color: 'white'}}>
+              {alertText.message}
+          </Alert>
+      </Snackbar>
+      </Stack>
+    );
+  };
+
+  // https://prizes.gamee.com/game-bot/kiBJvHa-caba58da853c651867ff4f5d5a2567bf5604beb7#tgShareScoreUrl=tgb%3A%2F%2Fshare_game_score%3Fhash%3DxHDZtNsHehnCzKxYeFIG
+
+
+  const onUpdateScoreClick = async () => {
+    console.log(gameUrl);
+    console.log(gameScore);
+
+    setLoading(true);
+
+    try {
+      const request = `${apiUrl}?api_key=${apiKey}&url=${gameUrl}&score=${gameScore}`;
+      console.log('Request:', request);
+
+      fetch(`${apiUrl}?api_key=${apiKey}&url=${gameUrl}&score=${gameScore}`, 
+        { 
+          method: 'POST', 
+          redirect: 'follow',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(response => response.json())
+          .then(data => {
+              if (data.status === 200) {
+                console.log('Success:', data.message);
+
+                setOpen(true);
+                setAlertText({ message: data.message, status: 'success' });
+
+              } else if (data.status === 400) {
+                console.error('Error:', data.message);
+                
+                setOpen(true);
+                setAlertText({ message: data.message, status: 'error' });
+              }
+          })
+          .catch(error => {
+              console.error('Error:', error);
+              
+              setOpen(true);
+              setAlertText({ message: 'Something went wrong', status: 'error' });
+          });
+      } catch (error) {
+          console.error('Error:', error);
+          
+          setOpen(true);
+          setAlertText({ message: 'Something went wrong', status: 'error' });
+      }
+    
+    setLoading(false);
+
+  };
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(teal[600]),
+    backgroundColor: teal[600],
+    padding: '15px 40px',
+    marginTop: '40px',
+    borderRadius: '5px',
+    '&:hover': {
+      backgroundColor: teal[900],
+    },
+    '&:disabled': {
+        backgroundColor: teal[400],
+        color: theme.palette.getContrastText(teal[400]),
+        cursor: 'not-allowed',
+    }
+  }));
+
+
+  return (
+    <div 
+      className="App"
+    >
+      <div 
+        className="App-header"
+      >
+        <div 
+          className="App-title">
+            Telegram Gamee Hack
+        </div>
+        {open ? Stackbar() : <></>}
+        <div 
+          className=''
+          style={{
+            width: '60%',
+            // backgroundColor: '#21252B',
+            marginTop: '60px',
+            padding: '40px 40px',
+            borderRadius: '5px',
+          }}
+        >
+            <form
+              onSubmit={handleSubmit}
+            >
+              <div 
+                className="clear-button"
+              >
+                <input
+                  className='input-border'
+                  value={gameUrl}
+                  style={{
+                    width: '80%',
+                    padding: '20px 30px',
+                    fontSize: '16px',
+                    borderRadius: '5px',
+                    resize: 'none',
+                    backgroundColor: '#F3F3F8',
+                    color: '#040404',
+                    outline: 'none',
+                  }}
+                  placeholder='Enter gamee url here'
+                  onChange={(e) => setGameUrl(e.target.value)}
+                >
+                </input>
+                <CloseIcon 
+                  style={{
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => setGameUrl('')}
+                /> 
+              </div>
+              <div
+                className="clear-button"
+              >
+                <input
+                  className='input-border'
+                  value={gameScore}
+                  style={{
+                    width: '80%',
+                    padding: '20px 30px',
+                    fontSize: '16px',
+                    borderRadius: '5px',
+                    resize: 'none',
+                    backgroundColor: '#F3F3F8',
+                    color: '#040404',
+                    outline: 'none',
+                  }}
+                  placeholder='Enter desired score here'
+                  onChange={(e) => setGameScore(e.target.value)}
+                >
+                </input>
+                <CloseIcon 
+                  style={{
+                    cursor: 'pointer',
+                  }}
+                  onClick={() => setGameScore('')}
+                />
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  gap: '20px',
+                }}
+              >
+                <ColorButton 
+                  disabled={loading || gameUrl.length === 0 || gameScore.length === 0}
+                  variant="contained" 
+                  type="submit"
+                  onClick={() => onUpdateScoreClick()}
+                  >
+                    Hack
+                </ColorButton>
+              </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
